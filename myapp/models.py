87b661386709b8a88ffe9aacaa16e9c04e17a508
Chandu <yarrapothusaichandu@gmail.com>
2024-10-25 14:28:40 +0530

    initial commit

---
+++ myapp/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Chat(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    response = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username}: {self.message}'
+
+
+class Profile(models.Model):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    ]
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    mobile_number = models.CharField(max_length=15, blank=True, null=True)  # Optional field
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True, null=True)  # Optional field
+    nationality = models.CharField(max_length=50, blank=True, null=True)  # Optional field
+
+    def __str__(self):
+        return self.user.username
+
+# Signal to automatically create Profile when a User is created
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
